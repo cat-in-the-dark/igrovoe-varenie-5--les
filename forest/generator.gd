a00b0fb5dc9a1168e02ed38a0b0d61ea66c8cbd2
Ilya Siganov <ilya.blan4@gmail.com>
2026-01-05 17:33:54 +0600

    add basic movement in generated piece of forest

---
+++ forest/generator.gd
@@ -0,0 +1,30 @@
+extends Node
+
+@export var prefab: PackedScene
+@export var size: float = 30
+@export var spacing: float = 4
+@export var jitter: float = 1
+
+func generate_points(x: float, y: float) -> Array[Vector2]:
+	var points: Array[Vector2] = []
+	for xi in range(x, x+size, spacing):
+		for yi in range(y, y+size, spacing):
+			points.append(Vector2(
+				xi + jitter * spacing * (randf() - 0.5),
+				yi + jitter * spacing * (randf() - 0.5)
+			))
+	return points
+
+func spawn_object(points: Array[Vector2]):
+	for i in len(points):
+		var point = points[i]
+		var obj: Node3D = prefab.instantiate()
+		self.add_child(obj)
+		obj.position = Vector3(point.x, 0, point.y)
+		if i % 5 == 0:
+			# next frame for performance
+			await get_tree().process_frame
+
+func  _ready() -> void:
+	var points = generate_points(-size/2.0, -size/2.0)
+	spawn_object(points)
---
+++ player/player.gd
@@ -0,0 +1,39 @@
+class_name Player
+extends CharacterBody3D
+
+@onready var _camera: Camera3D = $Camera3D
+@export_range(0.0, 10.0) var joystick_sensitivity := 2.5
+@export_range(0.0, 1.0) var mouse_sensitivity := 0.25
+
+func _move_cmd() -> Vector2:
+	return Input.get_vector("move_left", "move_right", "move_forward", "move_back", 0.4)
+
+func _camera_cmd() -> Vector2:
+	return Input.get_vector("camera_left", "camera_right", "camera_up", "camera_down", 0.2)
+	
+func _handle_camera_joystic_move() -> Vector3:
+	var raw_input := self._camera_cmd()
+	var _camera_input_direction := Vector2.ZERO
+	_camera_input_direction.x = -raw_input.x * joystick_sensitivity
+	#_camera_input_direction.y = -raw_input.y * joystick_sensitivity
+	return Vector3(_camera_input_direction.y, _camera_input_direction.x, 0)
+
+func _handle_move_direction() -> Vector3:
+	var move_input := self._move_cmd()
+	var forward := _camera.global_basis.z
+	var right := _camera.global_basis.x
+	var move_direction := forward * move_input.y + right * move_input.x
+	move_direction.y = 0.0
+	move_direction = move_direction.normalized()
+	return move_direction
+
+func _process(delta: float) -> void:
+	var rot_dir = _handle_camera_joystic_move()
+	self.global_rotation += rot_dir * delta
+	
+	var move_dir = _handle_move_direction()
+	
+	move_dir.y = velocity.y
+	velocity = velocity.move_toward(move_dir * 4.0, 20.0 * delta)
+	
+	move_and_slide()
